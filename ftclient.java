225b49990f6d13295763cc347a84f56dc4d3274d
katiejyoung <kjy3@georgetown.edu>
2020-03-04 07:03:07 -0800

    Initialize project

---
+++ ftclient.java
@@ -0,0 +1,9 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class ftclient {
+    public static void main(String []args) throws IOException {
+        System.out.print("Client initiated\n");
+    }
+}
